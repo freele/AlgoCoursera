835d8a92b692dc49f520f3e18d7752c82267df31
freele <kremenets0207@gmail.com>
2013-02-10 18:03:18 +0400

    q3

--- AlgoCoursera/Algo2_PA3Q1.pl
+++ AlgoCoursera/Algo2_PA3Q1.pl
@@ -5,3 +5,52 @@ use strict;
 #[value_1] [weight_1]
 #[value_2] [weight_2]
 
+open InputInfo, 'Algo2_PA3Q1_input.txt';
+ 
+ my @SizeAndNumber = split (/\s+/, <InputInfo>);
+ 
+ my $knapsack_size = $SizeAndNumber[0];
+ my $number_of_items = $SizeAndNumber[1];
+ print "size = $knapsack_size, number 0f items = $number_of_items  \n";
+ 
+ #Subroutines
+ sub max {
+	my ($max, @vars) = @_;
+	for (@vars) {
+		$max = $_ if $_ > $max;
+	}
+	return $max;
+}
+
+ #Reading input
+ my @weights;
+ my @values;
+ 
+ foreach ( 1..$number_of_items)
+ {
+ 	my @temp = split (/\s+/, <InputInfo> );
+
+	$values[$_] = $temp[0];
+	$weights[$_] = $temp[1];
+ }
+ 
+ #Main algorithm
+my @A;
+foreach ( 0..$knapsack_size){
+	$A[0][$_] = 0;
+}
+
+foreach my $i  ( 1..$number_of_items){
+	foreach my $temp_w ( 0..$knapsack_size){
+		if ( $temp_w >= $weights[$i]){
+#			print "case1 \n";
+			$A[$i][$temp_w] = &max ( $A[$i-1][$temp_w], ($A[$i-1][$temp_w - $weights[$i]] + $values[$i]) )
+		}
+		else{
+#			print "case2 \n";
+			$A[$i][$temp_w] = $A[$i-1][$temp_w];
+		}
+#		print "i=$i, w=$temp_w, A=$A[$i][$temp_w] \n";
+	}
+ }
+ print "A=$A[-1][-1] \n";
---
+++ AlgoCoursera/Algo2_PA3Q2.pl
@@ -0,0 +1,76 @@
+#!usr/bin/perl -w
+use strict;
+
+#[knapsack_size][number_of_items]
+#[value_1] [weight_1]
+#[value_2] [weight_2]
+
+open InputInfo, 'Algo2_PA3Q2_input.txt';
+ 
+ my @SizeAndNumber = split (/\s+/, <InputInfo>);
+ 
+ my $knapsack_size = $SizeAndNumber[0];
+ my $number_of_items = $SizeAndNumber[1];
+ my %BVFW = ();
+ print "size = $knapsack_size, number 0f items = $number_of_items  \n";
+ 
+ 
+ #Reading input
+ my @weights;
+ my @values;
+ foreach ( 1..$number_of_items)
+ {
+ 	my @temp = split (/\s+/, <InputInfo> );
+
+	$values[$_] = $temp[0];
+	$weights[$_] = $temp[1];
+ }
+ 
+ #Subroutines
+ sub max {
+	my ($max, @vars) = @_;
+	for (@vars) {
+		$max = $_ if $_ > $max;
+	}
+	return $max;
+}
+
+sub best_value_for_weight{
+	my ( $item, $weight ) = @_;
+	print "weight = $weight item = $item\n";
+	if ( !exists $BVFW{$weight} ){
+		if ($weight >= $weights[$item]){
+			$BVFW{"$weight"} = &max ( best_value_for_weight ($item - 1, $weight), best_value_for_weight ($item - 1, $weight - $weights[$item]) + $values[$item] );
+		}
+		else{
+			if ($item == 1){
+				$BVFW{"$weight"} = 0;
+			}
+			else{
+			$BVFW{"$weight"} = best_value_for_weight ($item - 1, $weight);
+			}
+		}
+	}
+
+	return $BVFW{"$weight"};
+}
+
+ 
+ #Main algorithm
+my @A;
+foreach ( 0..$knapsack_size){
+	$A[0][$_] = 0;
+}
+
+#foreach my $i  ( 1..$number_of_items){
+	best_value_for_weight ($number_of_items, $knapsack_size);
+#	foreach my $temp_w ( 0..$knapsack_size){
+#	if ( $temp_w >= $weights[$i]){
+#		$A[$i][$temp_w] = &max ( $A[$i-1][$temp_w], ($A[$i-1][$temp_w - $weights[$i]] + $values[$i]) )
+#	}else{
+#		$A[$i][$temp_w] = $A[$i-1][$temp_w];
+#	}
+#	print "i=$i, w=$temp_w, A=$A[$i][$temp_w] \n";
+#}
+#}
+ print "A=$A[-1][-1] \n";
